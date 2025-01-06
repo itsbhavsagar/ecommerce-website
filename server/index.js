c2b64126757004ae7222ff7e6edcc7b05106eec7
Bhavsagar <bhavsagar92@gmail.com>
2025-01-07 02:51:24 +0530

    added nodemon

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  res.end('getting res from NodeJs');
+});
 
 server.listen(8080, () => {
   console.log('server is running on 8080');
