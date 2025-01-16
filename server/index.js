ba2f600ebbf4f1f6706a53c2e44892278fd6f257
Bhavsagar <bhavsagar92@gmail.com>
2025-01-16 23:01:55 +0530

    feat: Implemented server routes for product data and query handling
    - Added route to serve the main HTML file at the root endpoint.
    - Added route to return product data in JSON format at `/product`.
    - Added route to handle query parameters and return them in JSON format at `/query`.
    - Configured the server to listen on port 8080.

--- server/index.js
+++ server/index.js
@@ -8,23 +8,9 @@ let data = JSON.parse(fs.readFileSync('./Data.json', 'UTF-8')).products;
 const server = express();
 
 server.get('/', (req, res) => {
-  res.sendFile('/Users/sagar/Desktop/ecommerce-website/server/index.html');
+  res.sendFile(htmlFile);
 });
 
-server.get('/product', (req, res) => {
-  res.json(data);
-});
-
-server.get('/product/:id', (req, res) => {
-  let id = req.params.id;
-  let prodData = data.find((obj) => obj.id == id);
-  let { title, category, description } = prodData;
-  let modifiedHTML = htmlFile
-    .replace('**title**', title)
-    .replace('**category**', category)
-    .replace('**description**', description);
-  res.end(modifiedHTML);
-});
 server.get('/product', (req, res) => {
   res.json(data);
 });
