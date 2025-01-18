882e4b2423e8c6e10aef0f2e32fff6e82560d9b5
Bhavsagar <bhavsagar92@gmail.com>
2025-01-18 20:37:23 +0530

    feat: Add routes for adding and deleting products

--- server/index.js
+++ server/index.js
@@ -1,15 +1,5 @@
-import fs from 'fs';
-import express from 'express';
-import path from 'path';
-
-const app = express();
-const PORT = 8080;
-
-const htmlFilePath = path.join(__dirname, 'index.html');
-const dataFilePath = path.join(__dirname, 'Data.json');
-
 const htmlFile = fs.readFileSync(htmlFilePath, 'UTF-8');
-const data = JSON.parse(fs.readFileSync(dataFilePath, 'UTF-8')).products;
+let data = JSON.parse(fs.readFileSync(dataFilePath, 'UTF-8')).products;
 
 app.get('/', (req, res) => {
   res.sendFile(htmlFilePath);
@@ -22,6 +12,30 @@ app.get('/product', (req, res) => {
 app.get('/query', (req, res) => {
   console.log(req.query);
   res.json(req.query);
+});
+
+// Add a new product
+app.post('/product', express.json(), (req, res) => {
+  const newProduct = req.body;
+  data.push(newProduct);
+  fs.writeFileSync(
+    dataFilePath,
+    JSON.stringify({ products: data }, null, 2),
+    'UTF-8'
+  );
+  res.status(201).json(newProduct);
+});
+
+// Delete a product by id
+app.delete('/product/:id', (req, res) => {
+  const productId = req.params.id;
+  data = data.filter((product) => product.id !== productId);
+  fs.writeFileSync(
+    dataFilePath,
+    JSON.stringify({ products: data }, null, 2),
+    'UTF-8'
+  );
+  res.status(204).send();
 });
 
 app.listen(PORT, () => {
